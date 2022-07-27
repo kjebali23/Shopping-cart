7b1b9199b9d3c704e78d5d77b42de8b821bb83cd
kjebali23 <khalil.jebali.kj@gmail.com>
2022-07-27 15:11:57 +0100

    Created store page UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import Store from "./pages/Store"
 import About from "./pages/About"
 import Navbar from "./components/Navbar"
+// import {ShoppingCartProvider} from "../src/context/ShoppingCartContext"
 
 function App() {
 
